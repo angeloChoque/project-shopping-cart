31686409895dd1f2b73b19c8777f0b7004cd6efa
angelo <ancricoma_angelo@hotmail.com>
2024-07-01 10:12:05 -0500

    create login

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,19 +1,74 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const newUser = new User({
       ...req.body,
       password: hashedPassword,
     });
+
     await newUser.save();
-    return res.json(newUser);
+
+    jwt.sign(
+      {
+        id: newUser._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) console.log(err);
+        res.cookie("token", token);
+        res.json(newUser);
+      }
+    );
   } catch (error) {
     console.log(error);
     return res.status(500).send("error!!");
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    jwt.sign(
+      {
+        id: userFound._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) console.log(err);
+        res.cookie("token", token);
+        res.json(userFound);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("error!!");
+  }
+};
+
+export const logout = (req,res) => {
+  res.cookie("token","",{
+    expires : new Date(0)
+  })
+  return res.sendStatus(200)
+}
