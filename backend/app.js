ceee60a678fe0ea0e8f95b6d0e988bc908af13ab
angelo <ancricoma_angelo@hotmail.com>
2024-11-05 22:43:15 -0500

    news modified

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,20 @@
 import cors from "cors";
 import express from "express";
-import cookieParser from 'cookie-parser'
-import ProductRoutes from './routers/products.routes.js'
-import authRoutes from './routers/users.routes.js'
+import cookieParser from "cookie-parser";
+import ProductRoutes from "./routers/products.routes.js";
+import authRoutes from "./routers/users.routes.js";
 
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   credentials: true,
 };
 
-
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use("/api",authRoutes)
-app.use("/api",ProductRoutes)
-
+app.use("/api", authRoutes);
+app.use("/api", ProductRoutes);
 
 export default app;
